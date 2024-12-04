8c993494ffb9095b231c4f7d8cab53988f62c589
Elena Rubilova <yelena.rubilova@gmail.com>
2024-12-04 16:14:06 -0500

    Initial Commit

---
+++ LeetCodeCrackers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LeetCodeCrackers
+//
+//  Created by Elena Rubilova on 12/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeetCodeCrackers/LeetCodeCrackersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeCrackersApp.swift
+//  LeetCodeCrackers
+//
+//  Created by Elena Rubilova on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeCrackersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
