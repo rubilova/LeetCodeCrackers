ee2b7a6f04f988d644db1304db8e549cbaca488b
Elena Rubilova <yelena.rubilova@gmail.com>
2025-04-04 14:21:36 -0400

    Add DFS Array binary tree conversion

---
+++ LeetCodeCrackers/BFS_DFS/DFSArray.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+let root = TreeNode(1,
+    TreeNode(2,
+        TreeNode(4,
+            TreeNode(8),
+            TreeNode(9)
+        ),
+        TreeNode(5,
+            TreeNode(10),
+            TreeNode(11)
+        )
+    ),
+    TreeNode(3,
+        TreeNode(6,
+            TreeNode(12),
+            TreeNode(13)
+        ),
+        TreeNode(7,
+            TreeNode(14),
+            TreeNode(15)
+        )
+    )
+)
+
+func dfs(_ node: TreeNode?) -> [Int] {
+    var result: [Int] = []
+    guard let node = node else { return []}
+    result.append(node.val)   // Visit root
+    result += dfs(node.left)            // Traverse left
+    result += dfs(node.right)           // Traverse right
+    return result
+}
+
+var greeting = "Find an array of all values in a binary tree using DFS"
+let array = dfs(root)
